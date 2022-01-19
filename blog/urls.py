b6f3019c547bb465c7dccfe8dc22a751491a5b12
Ankit <ankitsawho@gmail.com>
2022-01-19 20:07:19 +0530

    static added

--- blog/urls.py
+++ blog/urls.py
@@ -4,15 +4,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('summernote/', include('django_summernote.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
